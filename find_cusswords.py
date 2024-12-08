24b14e5370369878a5b09edcc31544e33fe8893e
Luke-Fanguna <lukefanguna@gmail.com>
2024-12-08 13:28:50 -0800

    added scores for evalution

--- find_cusswords.py
+++ find_cusswords.py
@@ -1,6 +1,27 @@
+from detoxify import Detoxify
 import pandas as pd
 
-utterance = pd.read_csv('utterances.csv').dropna(axis=0, how='any')
-utterance['text'] = utterance['text'].str.lower()
-print(utterance[utterance['text'].str.contains('shit')])
-print(utterance['text'][393174])
+def reason(result):
+    val = 0
+    label = ""
+    for k, v in result.items():
+        if val < v:
+            label = k
+            val = v
+    return label, val
+
+new = pd.read_csv('outputs1.csv').fillna({'reason':""})
+# _labels = labels[labels['predictions'] == True].head()
+# print('second')
+# print(_labels)
+# scores = []
+# for l in _labels['text']:
+#     results = Detoxify('original').predict(l)
+#     _, score = reason(results)
+#     scores.append(score)
+# _labels['score'] = scores
+# combined_df = pd.merge(labels, _labels, how='outer', on=['text'])
+# combined_df.to_csv('scores.csv',columns=['predictions_x','reason_x','text','score'], index=False)
+
+
+
--- toxic_bert.py
+++ toxic_bert.py
@@ -1,16 +1,30 @@
 from detoxify import Detoxify
 import pandas as pd
 
+def reason(result):
+    val = 0
+    label = ""
+    for k, v in result.items():
+        if val < v:
+            label = k
+            val = v
+    return label
+
+start = len(pd.read_csv('outputs.csv'))
 df = pd.read_csv('utterance2324.csv').dropna(axis=0, how='any')
-output = []
-for t in df['text']:
+for t in df['text'][start:]:
+    d = pd.read_csv('outputs.csv')
     results = Detoxify('original').predict(t)
-    if max(results.values()) > .04:
-        output.append(True)
-    else:
-        output.append(False)
-df['LABEL'] = output
-df.to_csv('labelled_utterances.csv', index=False)
+    prediction = max(results.values()) > 0.04
+    label = ""
+    if prediction:
+        label = reason(results)
+    new_row = pd.DataFrame({'predictions': [prediction], 'reason' : label, 'text': [t]})
+    d = pd.concat([d, new_row], ignore_index=True)
+    d.to_csv('outputs.csv', index=False)
+
+# df['LABEL'] = output
+# df.to_csv('labelled_utterances.csv', index=False)
 # results = Detoxify('original').predict("next slide, please. you can skip the video. it doesn't matter. it's just basically anderson calling in preparation for this. it's okay. it's basically anderson calling me a dipshit because this is what my students do for fun. so you've already talked about non consensual sexual imagery. this stuff is being used. you have to understand that this technology is not hard to use. for most people, it's just a matter of going to a website.")
 # print(results)
 # print((df>.04).any())
