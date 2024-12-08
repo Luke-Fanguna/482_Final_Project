173e356a28e7d051cd74b5a7e7a4a4dfbb0c20c3
Luke-Fanguna <lukefanguna@gmail.com>
2024-12-08 13:31:22 -0800

    created scores

--- find_cusswords.py
+++ find_cusswords.py
@@ -10,18 +10,16 @@ def reason(result):
             val = v
     return label, val
 
-new = pd.read_csv('outputs1.csv').fillna({'reason':""})
-# _labels = labels[labels['predictions'] == True].head()
-# print('second')
-# print(_labels)
-# scores = []
-# for l in _labels['text']:
-#     results = Detoxify('original').predict(l)
-#     _, score = reason(results)
-#     scores.append(score)
-# _labels['score'] = scores
-# combined_df = pd.merge(labels, _labels, how='outer', on=['text'])
-# combined_df.to_csv('scores.csv',columns=['predictions_x','reason_x','text','score'], index=False)
+labels = pd.read_csv('outputs.csv').fillna({'reason':""})
+_labels = labels[labels['predictions'] == True].head()
+scores = []
+for l in _labels['text']:
+    results = Detoxify('original').predict(l)
+    _, score = reason(results)
+    scores.append(score)
+_labels['score'] = scores
+combined_df = pd.merge(labels, _labels, how='left', on=['text']).fillna({'score':0,'reason':''})
+combined_df.to_csv('scores.csv',columns=['predictions_x','reason_x','text','score'], index=False)
 
 
 
