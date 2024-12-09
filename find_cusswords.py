cec7aeb850c2366b3cf2d85c518a250fe83d75a0
Luke-Fanguna <lukefanguna@gmail.com>
2024-12-08 16:19:03 -0800

    UI version 1 finished for toxic phenom

--- find_cusswords.py
+++ find_cusswords.py
@@ -1,9 +1,108 @@
 import pandas as pd
+import sys
+import mysql.connector
+import os
+from dotenv import load_dotenv
+import datetime
+from detoxify import Detoxify
+load_dotenv()
 
 
+def get_discussion(did):
+    table = pd.read_csv('utterance2324.csv')
+    discussion = table[table['did'] == float(did)]
+    return discussion
+
+def find_phenom(table):
+    uid = []
+    for i, t in enumerate(table['text']):
+        results = Detoxify('original').predict(t)
+        high = max(results.values()) > .1
+        if high:
+            uid.append(table.iloc[i]['uid'])
+    return uid
+
+def get_metadata(did):
+    sql = """
+    SELECT 
+        bid, lastTouched
+    FROM
+        BillDiscussion
+    WHERE did = %s;
+    """
+
+    try:
+        db = mysql.connector.connect(
+            host=os.getenv("SQL_DB_host"),
+            user=os.getenv("SQL_DB_user"),
+            password=os.getenv("SQL_DB_password"),
+            database=os.getenv("SQL_DB_database")
+        )
+        cur = db.cursor()
+        cur.execute(sql, (did,))
+
+        bill, date = cur.fetchall()[0]
+        date = date.strftime('%m/%d/%Y')
+        print("Bill discussed:", bill)
+        print("Date of discussion:", date)
+
+        db.close()
+
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        return None, None
+
+def get_person(pid):
+    sql = """
+    SELECT 
+        first, last
+    FROM Person
+    WHERE pid = %s;
+    """
+
+    try:
+        db = mysql.connector.connect(
+            host=os.getenv("SQL_DB_host"),
+            user=os.getenv("SQL_DB_user"),
+            password=os.getenv("SQL_DB_password"),
+            database=os.getenv("SQL_DB_database")
+        )
+        cur = db.cursor()
+        cur.execute(sql, (pid,))
+
+        first, last = cur.fetchall()[0]
+        db.close()
+
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        return None, None
+
+    return first + " " + last
 
 
+def get_label(text):
+    scores = pd.read_csv('scores.csv')
+    label = scores[scores['text'] == text]['reason'].iloc[0]
+    return label
 
 
+def create_phenom(uids):
+    # TODO: Format as { PERSON said THIS, flagged as LABEL }
+    utterance = pd.read_csv('utterance2324.csv')
+    for uid in uids:
+        person = get_person(utterance[utterance['uid'] == uid]['pid'].iloc[0])
+        text = utterance[utterance['uid'] == uid]['text'].iloc[0]
+        label = get_label(text)
 
+        print(f'{person} was tagged as {label} for saying {text}')
 
+def main():
+    did = sys.argv[1]
+    get_metadata(did)
+    table = get_discussion(did)
+    uids = find_phenom(table)
+    if uids:
+        create_phenom(uids)
+
+if __name__ == '__main__':
+    main()
--- toxic_bert.py
+++ toxic_bert.py
@@ -22,9 +22,3 @@ for t in df['text']:
     new_row = pd.DataFrame({'predictions': [prediction], 'reason' : label, 'text': [t], 'score': val})
     d = pd.concat([d, new_row], ignore_index=True)
     d.to_csv('scores.csv', index=False)
-
-# df['LABEL'] = output
-# df.to_csv('labelled_utterances.csv', index=False)
-# results = Detoxify('original').predict("next slide, please. you can skip the video. it doesn't matter. it's just basically anderson calling in preparation for this. it's okay. it's basically anderson calling me a dipshit because this is what my students do for fun. so you've already talked about non consensual sexual imagery. this stuff is being used. you have to understand that this technology is not hard to use. for most people, it's just a matter of going to a website.")
-# print(results)
-# print((df>.04).any())
